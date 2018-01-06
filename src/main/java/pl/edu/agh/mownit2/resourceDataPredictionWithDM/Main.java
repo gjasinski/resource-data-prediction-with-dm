4f15defb2146b2201d1680bdc7e2e55707ba0d0a
Kornelia Rohulko <korneliarohulko@gmail.com>
2018-01-06 22:17:26 +0100

    Data Generator for ARFF file

--- src/main/java/pl/edu/agh/mownit2/resourceDataPredictionWithDM/Main.java
+++ src/main/java/pl/edu/agh/mownit2/resourceDataPredictionWithDM/Main.java
@@ -2,6 +2,7 @@ package pl.edu.agh.mownit2.resourceDataPredictionWithDM;
 
 public class Main {
     public static void main(String[] args) {
-
+        DataGenerator dataGenerator = new DataGenerator(15);
+        System.out.println(dataGenerator.getData());
     }
 }
