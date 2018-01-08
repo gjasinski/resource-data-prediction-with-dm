650a0b4e91a0163284ce7116d34ad27d32b0fd2e
Grzegorz Jasiński <gjasinski@hotmail.com>
2018-01-08 22:26:55 +0100

    working draft

--- src/main/java/pl/edu/agh/mownit2/resourceDataPredictionWithDM/Main.java
+++ src/main/java/pl/edu/agh/mownit2/resourceDataPredictionWithDM/Main.java
@@ -2,7 +2,7 @@ package pl.edu.agh.mownit2.resourceDataPredictionWithDM;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        DataGenerator dataGenerator = new DataGenerator(10);
+        DataGenerator dataGenerator = new DataGenerator(1);
         //System.out.println(dataGenerator.getData());
         DataPrediction dataPrediction = new DataPrediction();
     }
